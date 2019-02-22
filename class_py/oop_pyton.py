b0953e7e9b1b4986a834769bf40f56fb8c14dd9d
wangzhefeng <wangzhefengr.@163.com>
2019-02-23 01:02:51 +0800

    add somethings

---
+++ class_py/oop_pyton.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author:
+@date:
+"""
+
+
+class FirstClass(object):
+	def setdata(self, value):
+		self.name = value
+
+	def display(self):
+		print(self.name)
+
+
+
+class SecondClass(FirstClass):
+	def display(self):
+		print("Current value = %s" % self.data)
+
+
+
+def main():
+	x = FirstClass()
+	y = FirstClass()
+	x.setdata("wangzhefeng")
+	y.setdata(3.14159)
+	x.display()
+	y.display()
+
+	x.data = "New Value"
+	x.display()
+
+	x.anothername = "spam"
+
+
+
+if __name__ == "__main__":
+	main()
