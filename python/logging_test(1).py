bc89209ea6fdbb55db803c71d3afb12a0d2c9c78
wangzhefeng <wangzhefengr@163.com>
2019-08-24 12:11:20 +0800

    fix

---
+++ python/logging_test(1).py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-08-16 10:09:56
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import logging 
+logging.basicConfig(filename = "F:/personal/script/log/test.log",
+                    filemode = "w",
+                    format = "[%(asctime)s] 【%(name)s:%(levelname)s】: %(message)s", 
+                    datefmt="%Y-%m-%d %H:%M:%S",
+                    style = "%",
+                    level = "DEBUG",
+                    # stream = "",
+                    # handles = ""
+                    )
+
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+try:
+    a = 5
+    b = 0
+    c = a / b
+except ZeroDivisionError as e:
+    logging.exception("Exception occurred")
+    logging.error("Exception occurred", 
+                  exc_info = True)
+    logging.log(level = logging.DEBUG, 
+                msg = "Exception occurred", 
+                exc_info = True)
+
+
---
+++ python/python_knowledge.md
@@ -0,0 +1,8 @@
+* python 多进程，多线程
+* python logging
+* python 任务调度
+* python pandas timeseries
+* python class
+* 2to3.py
+	- `python 2to3.py -h`
+	- `python 2to3.py -w`
