43940b5692442c52c249e691824574d1738815c6
wangzhefeng <wangzhefengr@163.com>
2020-03-24 19:23:59 +0800

    test

---
+++ DesignPatten/CloneModel/CloneModel_V1.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from copy import copy, deepcopy
+
+
+# ----------------------------------------
+#
+# ----------------------------------------
+class Person:
+    """
+    人
+    """
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+    
+    def showMyself(self):
+        print("我是" + self.__name + "，年龄" + str(self.__age) + "。")
+    
+    def coding(self):
+        print("我是码农，我用程序改变世界，Coding......")
+
+    def reading(self):
+        print("阅读是我快乐！知识使我成长！如饥似渴地阅读是生活的一部分......")
+    
+    def fallInLove(self):
+        print("春风吹，月亮明，花前月下好相约.......")
+    
+    def clone(self):
+        return copy(self)
+
+def testClone():
+    zfwang = Person("zfwang", 30)
+    zfwang.showMyself()
+    zfwang.coding()
+
+    zfwang2 = zfwang.clone()
+    zfwang2.showMyself()
+    zfwang2.reading()
+
+    zfwang3 = zfwang.clone()
+    zfwang3.showMyself()
+    zfwang3.fallInLove()
+
+
+# ----------------------------------------
+# 
+# ----------------------------------------
+class PetStore:
+    """宠物店"""
+
+    def __init__(self, name):
+        self.__name = name
+        self.__petList = []
+    
+    def setName(self, name):
+        self.__name = name
+    
+    def showMyself(self):
+        print("%s 宠物店有以下宠物：" % self.__name)
+        for pet in self.__petList:
+            print(pet + "\t", end = "")
+        print()
+    
+    def addPet(self, pet):
+        self.__petList.append(pet)
+
+def testPetStore():
+    petter = PetStore("Petter")
+    petter.addPet("小狗Coco")
+    print("父本 petter：", end = "")
+    petter.showMyself()
+    print()
+
+    petter1 = deepcopy(petter)
+    petter1.addPet("小猫 Amy")
+    print("副本 petter1：", end = "")
+    petter1.showMyself()
+    print("父本 petter：", end = "")
+    petter.showMyself()
+    print()
+
+    petter2 = copy(petter)
+    petter2.addPet("小兔 Ricky")
+    print("副本 petter2：", end = "")
+    petter2.showMyself()
+    print("父本 petter：", end = "")
+    petter.showMyself()
+
+
+# ----------------------------------------
+#
+# ----------------------------------------
+def testList():
+    L = [1, 2, 3]
+    L1 = L
+    print("id(L):", id(L))
+    print("id(L1):", id(L1))
+    
+
+
+
+
+
+
+def main():
+    testClone()
+    testPetStore()
+
+if __name__ == "__main__":
+    main()
+
---
+++ python_attr_cattr_2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-# class 3
+
+
+from attr import attrs, attrib, fields, validators
+from cattr import structure, unstructure
+
+
+# class 1
+@attrs
+class Point(object):
+    x = attrib()
+    y = attrib()
+
+
+# class 2
+def is_valid_gender(instance, attribute, value):
+    if value not in ["male", "female"]:
+        raise ValueError(f"gender {value} is not vaild.")
+
+@attrs
+class Person(object):
+    name = attrib()
+    gender = attrib(validator = is_valid_gender)
+    age = attrib(validator = validators.instance_of(int))
+
+
+if __name__ == "__main__":
+    # 声明和比较
+    p1 = Point(x = 1, y = 2)
+    p2 = Point(x = 1, y =2)
+    print(p1)
+    print(p2)
+    print('Equal(eq):', Point(1, 2) == Point(1, 2))
+    print('Not Equal(ne):', Point(1, 2) != Point(3, 4))
+    print('Less Than(lt):', Point(1, 2) < Point(3, 4))
+    print('Less or Equal(le):', Point(1, 2) <= Point(1, 4), Point(1, 2) <= Point(1, 2))
+    print('Greater Than(gt):', Point(4, 2) > Point(3, 2), Point(4, 2) > Point(3, 1))
+    print('Greater or Equal(ge):', Point(4, 2) >= Point(4, 1))
+    # 属性定义
+    print(fields(Point))
+    # 属性名 `x = attrib()`
+    # 默认值 `default = 100`
+    # 初始化 `init = False`
+    # 强制关键字 `kw_only = True`
+    # 验证器
+    print(Person(name = "Mike", gender = "male", age = "18"))
+    print(Person(name = "Mike", gender = "mlae", age = 20))
+    # 转换器
+    # 类型
+    # 序列转换
+    # 基本转换
+    # 多类型转换
+    # 属性处理
+    # 嵌套处理
