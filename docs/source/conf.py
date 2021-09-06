3c647688539d2ca92e18d0374829ef8e0f23603a
wangzhefeng <wangzhefengr@163.com>
2021-09-06 22:25:54 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,13 +16,13 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 if not 'READTHEDOCS' in os.environ:
     sys.path.insert(0, os.path.abspath('..'))
 
 sys.path.insert(0, os.path.abspath('../..src/'))
-# sys.path.append(os.path.abspath('.Linux/'))
+
 
 # -- General configuration ------------------------------------------------
 
