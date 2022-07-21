067613b5dd8c422cbc1137f7952c919b69bab84c
wangzhefeng <wangzhefengr@163.com>
2022-07-22 00:27:29 +0800

    add pydask

---
+++ pysci/pydask/demo.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+
+# ***************************************************
+# * File        : demo.py
+# * Author      : Zhefeng Wang
+# * Email       : wangzhefengr@163.com
+# * Date        : 2022-07-22
+# * Version     : 0.1.072200
+# * Description : description
+# * Link        : link
+# * Requirement : 相关模块版本需求(例如: numpy >= 2.1.0)
+# ***************************************************
+
+
+# python libraries
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+import dask.dataframe as dd
+import dask.array as da
+import dask.bag as db
+
+
+# global variable
+LOGGING_LABEL = __file__.split('/')[-1][:-3]
+GLOBAL_VARIABLE = None
+
+
+
+
+
+
+# 测试代码 main 函数
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
--- pysci/tqdm_demo.py
+++ pysci/tqdm_demo.py
@@ -19,14 +19,16 @@ import time
 
 
 
+
+
+
+
+
 # 测试代码 main 函数
 def main():
     for i in tqdm(range(10), desc = "1st loop"):
         for j in trange(100, desc = "2nd loop", leave = False):
             time.sleep(0.01)
-
-
-
 
 
 if __name__ == "__main__":
